636dfe650a72dfab62ad5c78f2e350d4e3e24b07
andr1312e <andr1312e@gmail.com>
2020-01-30 19:57:11 +0300

    Добавьте файлы проекта.

---
+++ ApiControllers/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ApiControllers.Models;
+
+namespace ApiControllers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IRepository repository { set; get; }
+        // GET: Home
+        public HomeController(IRepository repo) 
+        {
+            repository = repo;
+        }
+        public ViewResult Index()
+        {
+            return View(repository.Reservations);
+        }
+        [HttpPost]
+        public IActionResult AddReservation(Reservation reservation)
+        {
+            repository.AddReservation(reservation);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ApiControllers/Models/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiControllers.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { set; get; }
+        public string ClientName { set; get; }
+        public string Location { set; get; }
+    }
+}
